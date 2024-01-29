525035c83229123b8eb3778042e7f3cf736d8dee
jerry <jerry@westrick.com>
2024-01-29 07:48:07 -0600

    KE-21 Updated OPEN AI Models and costs

--- OpenAI_API_Costs.py
+++ OpenAI_API_Costs.py
@@ -1,19 +1,16 @@
 OpenAI_API_Costs = {
-    'gpt-4-1106-preview':
-        {'model': 'gpt-4-1106-preview', 'title': 'GPT-4 Turbo',
-         'RPM': 200, 'TPM': 40000, 'input': 0.0100, 'output': 0.0300},
-    'gpt-4':
-        {'model': 'gpt-4', 'title': 'GPT-4 (8k)',
-         'RPM': 200, 'TPM': 40000, 'input': 0.0300, 'output': 0.0600},
-    'gpt-3.5-turbo-16k':
-        {'model': 'gpt-3.5-turbo-16k', 'title': 'gpt-3.5-turbo (16K)',
-         'RPM': 3500, 'TPM': 90000, 'input': 0.0030, 'output': 0.0040},
-    'gpt-3.5-turbo':
-        {'model': 'gpt-3.5-turbo', 'title': 'gpt-3.5-turbo (4K)',
-         'RPM': 3500, 'TPM': 90000, 'input': 0.0015, 'output': 0.0020},
-    'gpt-3.5-turbo-1106':
-        {'model': 'gpt-3.5-turbo-1106', 'title': 'gpt-3.5-turbo (4K)',
-         'RPM': 3500, 'TPM': 90000, 'input': 0.0015, 'output': 0.0020},
+
+    # GPT4 Turbo
+    'gpt-4-1106-preview': {'model': 'gpt-4-1106-preview', 'title': 'GPT-4 Turbo',      'input': 0.0100, 'output': 0.0300, 'context': 128000},
+    'gpt-4-0125-preview': {'model': 'gpt-4-0125-preview', 'title': 'GPT-4 Turbo(new)', 'input': 0.0100, 'output': 0.0300, 'context': 128000},
+
+    # GPT4
+    'gpt-4':              {'model': 'gpt-4',              'title': 'GPT-4 (8k)',  'input': 0.0300, 'output': 0.0600, 'context': 8000},
+    'gpt-4-32k':          {'model': 'gpt-4-32k',          'title': 'GPT-4 (32k)', 'input': 0.0600, 'output': 0.1200, 'context': 32000},
+
+    # GPT3
+    'gpt-3.5-turbo-1106':     {'model': 'gpt-3.5-turbo-1106',     'title': 'gpt-3.5-turbo (16K)',    'input': 0.0010, 'output': 0.0020, 'context': 16000},
+    'gpt-3.5-turbo-instruct': {'model': 'gpt-3.5-turbo-instruct', 'title': 'gpt-3.5-turbo-instruct', 'input': 0.0015, 'output': 0.0020, 'context': 4000},
 }
 
 
